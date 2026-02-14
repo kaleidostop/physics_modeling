fix: calculation error

app.py
@@ -10,11 +10,16 @@ eV = 1.602e-19
 
 def potential(x, kind, a, U0):
     if kind == "parabolic":
-        return -U0 * (x/a) * (1 - x/a)
+        return -U0 * (1 - (2*x/a)**2)
     if kind == "square":
-        return -U0 * np.ones_like(x)
+        U = np.zeros_like(x)
+        mask = np.abs(x) < a / 2
+        U[mask] = -U0
+        return U
     if kind == "double":
-        return -U0 * ((x - a/2)/(a/2))**4
+        return -U0 * (1 - (2*x / a)**2)**2
+    if kind == "arbitrary":
+        return -U0 * (x/a) * (10 - x/a)  # для произвольной формулы
     raise ValueError("Unknown potential")
 
 
@@ -22,12 +27,13 @@ def solve(a_nm, U0_ev, kind, kmax, N=200):
     a = a_nm * 1e-9
     U0 = U0_ev * eV
 
-    dx = a / (N + 1)
-    x = np.linspace(dx, a - dx, N)
+    L = 3 * a
+    dx = L / (N + 1)
+    x = np.linspace(-L / 2 + dx, L / 2 - dx, N)
 
     U = potential(x, kind, a, U0)
 
-    main = (hbar**2 / (2*m*dx**2)) + U
+    main = (hbar**2 / (m*dx**2)) + U
     off = -hbar**2 / (2*m*dx**2)
 
     H = (
@@ -44,7 +50,7 @@ def solve(a_nm, U0_ev, kind, kmax, N=200):
     energies_ev = E / eV
     U_ev = U / eV
 
-    bound_idx = np.where(energies_ev < np.max(U_ev))[0]
+    bound_idx = np.where(energies_ev < 0)[0]
 
     k = min(len(bound_idx), kmax)
     idx = bound_idx[:k]
@@ -54,7 +60,8 @@ def solve(a_nm, U0_ev, kind, kmax, N=200):
         "U": U_ev.tolist(),
         "energies": energies_ev[idx].tolist(),
         "psi": psi[:, idx].tolist(),
-        "n_states": len(bound_idx)
+        "n_states": len(bound_idx),
+        "k": k
     }
 
 @app.route("/")
@@ -69,7 +76,8 @@ def api():
             data["a"],
             data["U0"],
             data["kind"],
-            data["kmax"]
+            data["kmax"],
+            data["N"]
         )
     )
 